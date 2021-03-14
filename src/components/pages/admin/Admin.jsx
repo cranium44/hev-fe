3bcaebf3520f3e71d3ff8ec87d6e4ffaf2eb85fb
Adabe <adabe@scienta.co>
2021-03-14 17:53:28 +0100

    style admin page

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import "./Admin.css";
 
-
 // {
 //     id: {type: Number, required: true},
 //     name: {type: String, required: true},
@@ -11,10 +10,10 @@ import "./Admin.css";
 //     pkgSize: {type: String},
 // }
 const Admin = (props) => {
-  const [item, setItem] = useState({});
-  const [itemList, setItemList] = useState([]);
+    const [item, setItem] = useState({});
+    const [itemList, setItemList] = useState(null);
 
-  //fields
+    //fields
     const [serialNumber, setSerialNumber] = useState("");
     const [name, setName] = useState("");
     const [brand, setBrand] = useState("");
@@ -28,42 +27,78 @@ const Admin = (props) => {
             name,
             price,
             desc: brand,
-            pkgSize
-        })
-        console.log(item)      
-    }
+            pkgSize,
+        });
+        console.log(item);
+    };
 
-    useEffect(function () {
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("http://localhost:5000/test");
+            const data = await response.json();
+            setItemList(data);
+            console.log(data)
+        }
+        fetchData();
+    }, []);
 
-    },[])
-
-  return (
-    <div className="">
-      <form onSubmit={hanldeSubmit}>
-        <label for="sno" className="admin__label">
-          Serial number:
-        </label>
-        <input type="text" name="sno" className="admin__input" onChange={e => setSerialNumber(e.target.value)}/>
-        <label for="description" className="admin__label">
-          Item name:
-        </label>
-        <input type="text" name="description" className="admin__input" onChange={e => setName(e.target.value)}/>
-        <label for="brand" className="admin__label">
-          Brand/Code:
-        </label>
-        <input type="text" name="brand" className="admin__input"  onChange={e => setBrand(e.target.value)}/>
-        <label for="pkgSize" className="admin__label">
-          Package Size
-        </label>
-        <input type="text" name="pkgSize" className="admin__input" onChange={e => setSize(e.target.value)}/>
-        <label for="price" className="admin__label">
-          Unit price
-        </label>
-        <input type="text" name="price" className="admin__input" onChange={e => setPrice(e.target.value)}/>
-        <button type="submit" className="admin__send-btn">Submit</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="admin__container container">
+            <div className="admin__item-form col-5">
+                <form onSubmit={hanldeSubmit}>
+                    <label htmlFor="sno" className="admin__label">
+                        Serial number:
+                    </label>
+                    <input
+                        type="text"
+                        name="sno"
+                        className="admin__input"
+                        onChange={(e) => setSerialNumber(e.target.value)}
+                    />
+                    <label htmlFor="description" className="admin__label">
+                        Item name:
+                    </label>
+                    <input
+                        type="text"
+                        name="description"
+                        className="admin__input"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <label htmlFor="brand" className="admin__label">
+                        Brand/Code:
+                    </label>
+                    <input
+                        type="text"
+                        name="brand"
+                        className="admin__input"
+                        onChange={(e) => setBrand(e.target.value)}
+                    />
+                    <label htmlFor="pkgSize" className="admin__label">
+                        Package Size
+                    </label>
+                    <input
+                        type="text"
+                        name="pkgSize"
+                        className="admin__input"
+                        onChange={(e) => setSize(e.target.value)}
+                    />
+                    <label htmlFor="price" className="admin__label">
+                        Unit price
+                    </label>
+                    <input
+                        type="text"
+                        name="price"
+                        className="admin__input"
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                    <button type="submit" className="admin__send-btn">
+                        Submit
+                    </button>
+                </form>
+            </div>
+            <div className="admin__current-items col-5"></div>
+        </div>
+    );
 };
 
 export default Admin;
