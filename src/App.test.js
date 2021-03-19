616515d8cd09fa8af76a3aff4d85f174e2c244b2
Adabe <adabe@scienta.co>
2021-03-19 19:03:51 +0100

    fix test case
    Author:    Adabe <adabeechaim@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/healthcare/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getAllByText(/healthcare/i);
+  expect(linkElement.length).toBeGreaterThan(0);
 });
