b4ab281ab1b1d138c3ae5d1ec4393b64a00a5d1c
Adabe Echaim <adabeechaim@gmail.com>
2021-03-20 20:16:33 +0100

    refactor footer to read from json

--- src/components/util/footer/Footer.jsx
+++ src/components/util/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import data from "../../../static/socials.json";
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,24 +9,37 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer__container">
                 <div className="footer__branding col-5">
-                    <img src="/hev-logo.png" alt="logo" className="footer__logo"/>
-                    <span className="footer__branding-title">Healthcare Excellence Ventures</span>
+                    <img
+                        src="/hev-logo.png"
+                        alt="logo"
+                        className="footer__logo"
+                    />
+                    <span className="footer__branding-title">
+                        Healthcare Excellence Ventures
+                    </span>
                 </div>
                 <div className="footer__contact-info col-4">
                     <h4>Address</h4>
                     <p>Secretariat road, Gwagwalada</p>
                 </div>
                 <div className="footer__socials col-2">
-                <FontAwesomeIcon icon={["fab", "github"]} />
+                    <FontAwesomeIcon icon={["fab", "github"]} />
                     <ul className="footer__links">
-                        <li className="footer__link"><a href="#">Facebook</a></li>
-                        <li className="footer__link"><a href="#">Twitter</a></li>
-                        <li className="footer__link"><a href="#">Email</a></li>
+                        {data.links.map((link, index) => {
+                            return (
+                                <li className="footer__link" key={index}>
+                                    <a href={link.href}>{link.title}</a>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
             </div>
             <div className="footer__copyright">
-              <span>{new Date().getFullYear()}&#169; Healthcare Excellence Ventures</span>
+                <span>
+                    {new Date().getFullYear()}&#169; Healthcare Excellence
+                    Ventures
+                </span>
             </div>
         </footer>
     );
