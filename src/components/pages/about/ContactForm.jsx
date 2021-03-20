9b699af60a7b6a55688ff576ffd6c887b6a6387a
Adabe Echaim <adabeechaim@gmail.com>
2021-03-20 22:37:42 +0100

    styled contact form

--- src/components/pages/about/ContactForm.jsx
+++ src/components/pages/about/ContactForm.jsx
@@ -1,28 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AboutPage.css";
 
 const ContactForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        console.log({ name, email, message, phone });
+    };
+
     return (
-        <div className="about__contact-form-container">
-            <div className="about__contact-form-inputs col-8 ">
-                <input
-                    name="name"
-                    type="text"
-                    placeholder="Enter full name..."
-                />
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email address..."
-                />
-                <input
-                    name="phone"
-                    type="number"
-                    placeholder="Enter your email address..."
-                />
-            </div>
-            <textarea name="mesage" placeholder="Enter message..." itemType="text" rows="7" cols="100"/>
-            <button className="about__contact__btn">Send</button>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="about__contact-form-container">
+                    <div className="about__contact-form-inputs">
+                        <div className="form-group">
+                            <label
+                                htmlFor="name"
+                                className="form-control-label"
+                            >
+                                Full Name
+                            </label>
+                            <input
+                                className="form-control"
+                                value={name}
+                                onChange={(e) => {
+                                    setName(e.target.value);
+                                }}
+                                name="name"
+                                type="text"
+                                required
+                                placeholder="Enter full name..."
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label
+                                htmlFor="email"
+                                className="form-control-label"
+                            >
+                                Your Email
+                            </label>
+                            <input
+                                className="form-control"
+                                name="email"
+                                value={email}
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                }}
+                                type="email"
+                                required
+                                placeholder="Enter your email address..."
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label
+                                htmlFor="phone"
+                                className="form-control-label"
+                            >
+                                Phone
+                            </label>
+                            <input
+                                className="form-control"
+                                name="phone"
+                                value={phone}
+                                onChange={(e) => {
+                                    setPhone(e.target.value);
+                                }}
+                                type="phone"
+                                placeholder="Enter your phone number..."
+                            />
+                        </div>
+                    </div>
+                    <textarea
+                        className="form-control"
+                        name="mesage"
+                        value={message}
+                        onChange={(e) => {
+                            setMessage(e.target.value);
+                        }}
+                        placeholder="Enter message..."
+                        itemType="text"
+                        required
+                        rows="7"
+                        cols="100"
+                    />
+                    <button
+                        className="btn-contact btn-form btn-black-outline"
+                        type="submit"
+                    >
+                        Send
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
