c80c448f838708d928e1644b0d09cdb3e61c1f49
Adabe Echaim <adabeechaim@gmail.com>
2021-03-24 11:49:23 +0100

    add supplier to home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,13 +3,14 @@ import { default as Carousel } from "../../UIElements/carousel/Carousel";
 import { Jumbotron, Container } from "react-bootstrap";
 
 import data from "../../../static/home.json";
+import './Home.css'
 
 const Home = (pops) => {
     return (
         <div>
             <Carousel />
             <section>
-                <div>
+                <div className="home__details-container">
                     {data.data.map((item, index) => {
                         return (
                             <Jumbotron fluid key={index}>
@@ -20,6 +21,20 @@ const Home = (pops) => {
                             </Jumbotron>
                         );
                     })}
+                </div>
+            </section>
+            <section className="home__supplier-section">
+                <h3>We supply to</h3>
+                <div className="home__suppliers">
+                    {
+                        data.suppliers.map((supplier, index) => {
+                            return(
+                                <div className="home__supplier">
+                                    <img className="supplier-logo" src={supplier.imgSrc} alt={supplier.name}/>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </section>
         </div>
