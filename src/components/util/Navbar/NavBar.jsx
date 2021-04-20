178a34a433b09d674dd174d71d30768de73fc956
Adabe Echaim <adabeechaim@gmail.com>
2021-04-20 16:34:49 +0100

    fix navbar toggle

--- src/components/util/Navbar/NavBar.jsx
+++ src/components/util/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
 import "./NavBar.css";
 
 const NavBar = () => {
@@ -13,14 +13,6 @@ const NavBar = () => {
                     <Nav.Link href="/order">Order</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <FormControl
-                        type="text"
-                        placeholder="Search"
-                        className="mr-sm-2"
-                    />
-                    <Button variant="outline-info">Search</Button>
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
