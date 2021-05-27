2b24e75470f2bb44c3058ed8e5000d768059cc2e
Adabe Echaim <adabeechaim@gmail.com>
2021-05-27 20:27:02 +0100

    ♻️  code refactorig

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from "react";
 import {Card} from 'react-bootstrap'
 import {Grid} from '@material-ui/core'
+import OrderNav from './nav/OrderNav'
 
 import "./OrderPage.css";
 
 const OrderPage = (pops) => {
     const [items, setItems] = useState([])
+    const [selectedView, setSelectedView] = useState(0)
     useEffect(function () {
         const fetchData = async () => {
             try{
@@ -20,33 +22,36 @@ const OrderPage = (pops) => {
     },[])
 
     return (
-        <div className="container-fluid">
-            <div className="order__main-content">
-                <Grid container spacing="2" justify="space-evenly">
-                {
-                    items.map((item, index)=>{
-                        return(
-                            <Grid item xs={"auto"} key={index}>
-                                <Card style={{ width: '18rem' }} className="text-center">
-                                    <Card.Img variant="top" src="https://picsum.photos/200"/>
-                                    <Card.Title as="h4">{item.name}</Card.Title>
-                                    <Card.Text>
-                                        {
-                                            item.brand
-                                        }
-                                    </Card.Text>
-                                    <div className="item__card-control">
-                                        <img className="add-icon" src="/images/add-cart-50.png" alt="icon"/>
-                                    </div>
-                                </Card>
-                            </Grid>
-                        )
-                    })
-                }
-                </Grid>
+        <div className="container order__container">
+            <div className="col-md-2">
+                <OrderNav selectedView={selectedView} setSelectedView={setSelectedView}/>
+            </div>
+            <div className="order__main-content col-md-10">
+                {newFunction(items)}
             </div>
         </div>
     );
 };
 
 export default OrderPage;
+function newFunction(items) {
+    return <Grid container spacing="2" justify="space-evenly">
+        {items.map((item, index) => {
+            return (
+                <Grid item xs={"auto"} key={index}>
+                    <Card style={{ width: '18rem' }} className="text-center">
+                        <Card.Img variant="top" src="https://picsum.photos/200" />
+                        <Card.Title as="h4">{item.name}</Card.Title>
+                        <Card.Text>
+                            {item.brand}
+                        </Card.Text>
+                        <div className="item__card-control">
+                            <img className="add-icon" src="/images/add-cart-50.png" alt="icon" />
+                        </div>
+                    </Card>
+                </Grid>
+            );
+        })}
+    </Grid>;
+}
+
---
+++ src/components/pages/order/nav/OrderNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OrderNav = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>View All Items</li>
+                <li>View Cart</li>
+            </ul>
+        </div>
+    );
+}
+
+export default OrderNav;
